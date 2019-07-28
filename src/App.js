10f6baffcf49cf2aebb276f9d481525be4f2cc7e
ivansey <van.secret2015@gmail.com>
2019-07-28 20:33:04 +0300

    Reformat code

--- src/App.js
+++ src/App.js
@@ -9,26 +9,23 @@ import Footer from "./Footer";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <About/>
-      <StackFront/>
-      <StackBack/>
-      <Example/>
-      <Contact/>
-      <Footer/>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118751754-2"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'UA-118751754-2');
-        </script>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <About/>
+            <StackFront/>
+            <StackBack/>
+            <Example/>
+            <Contact/>
+            <Footer/>
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118751754-2"></script>
+            <script>
+                window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js',
+                new Date()); gtag('config', 'UA-118751754-2');
+            </script>
+        </div>
+    );
 }
 
 export default App;
