9bd4f04f8097399937173390707e6e2f08afe514
ivansey <van.secret2015@gmail.com>
2019-07-29 02:18:22 +0300

    Add Mini Header

--- src/App.js
+++ src/App.js
@@ -7,22 +7,47 @@ import StackBack from "./StackBack";
 import Example from "./Example";
 import Contact from "./Contact";
 import Footer from "./Footer";
+import MiniHeader from "./MiniHeader";
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <About/>
-            <Costs/>
-            <StackFront/>
-            <StackBack/>
-            <Example/>
-            <Contact/>
-            <Footer/>
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            miniHeader: false
+        };
+        this.updateClassHeader = this.updateClassHeader.bind(this);
+    }
 
-        </div>
-    );
+    static updateScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+    }
+
+    updateClassHeader(val) {
+        let scroll = App.updateScroll();
+        if (scroll >= val) {
+            this.setState({ miniHeader: true });
+        } else {
+            this.setState({ miniHeader: false });
+        }
+    }
+
+    render() {
+        setInterval(this.updateClassHeader, 100, 245);
+        return (
+            <div className="App">
+                <Header/>
+                <MiniHeader status={ this.state.miniHeader }/>
+                <About/>
+                <Costs/>
+                <StackFront/>
+                <StackBack/>
+                <Example/>
+                <Contact/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
