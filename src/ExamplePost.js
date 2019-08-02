47e8cd91a10f0653b3219738282275c7df48c86e
ivansey <van.secret2015@gmail.com>
2019-08-03 00:23:48 +0300

    Add support second and third url in ExamplePost

--- src/ExamplePost.js
+++ src/ExamplePost.js
@@ -7,6 +7,16 @@ class ExamplePost extends React.Component {
             <p className="desc">{this.props.desc}</p>
             <p className="tags">{this.props.tags}</p>
             <a href={this.props.url} target="_blank">{this.props.urlTitle}</a>
+            {
+              this.props.url2 !== ""
+                ? <a href={this.props.url2} target="_blank">{this.props.urlTitle2}</a>
+                : null
+            }
+            {
+              this.props.url3 !== ""
+                ? <a href={this.props.url3} target="_blank">{this.props.urlTitle3}</a>
+                : null
+            }
         </div>
     }
 }
