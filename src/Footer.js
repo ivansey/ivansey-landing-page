e993564002f50bb44d4f89d278cf313b2fd1e4f1
ivansey <van.secret2015@gmail.com>
2019-08-03 14:28:28 +0300

    Add text in Footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,6 +4,8 @@ class Footer extends React.Component {
     render() {
         return <div className="footer">
             <p>(c) Ivan Panasyuk - 2019</p>
+            <p>Создано с использванием фреймворка <a href="https://ru.reactjs.org/" target="_blank">React</a></p>
+            <p><a href="https://github.com/ivansey/ivansey-landing-page/">Исходный код на GitHub</a></p>
         </div>
     }
 }
