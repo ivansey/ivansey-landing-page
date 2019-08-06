73f775f07907a4ee226336fb072611dcc2728e9d
ivansey <van.secret2015@gmail.com>
2019-08-06 20:21:03 +0300

    Refactoring About

--- src/About.js
+++ src/About.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import StackFront from "./StackFront";
+import StackBack from "./StackBack";
 
 class About extends React.Component {
     render() {
-        return <div className="page">
-            <h1 className="title">Обо мне</h1>
-            <h2 className="subtitle">Иван Панасюк</h2>
-            <div className="text">Занимаюсь FullStack разработкой веб приложений. Тольк недавно начал занимать фрилансов и примеров работ немного</div>
-            <div className="text">Создаю лендинги, прочие одностраничные и многостраничные сайты, порталы, блоги, магазины, интересные сервисы и так далее</div>
+        return <div>
+            <div className="page">
+                <h1 className="title">Обо мне</h1>
+                <h2 className="subtitle">Иван Панасюк</h2>
+                <div className="text">Занимаюсь FullStack разработкой веб приложений. Тольк недавно начал занимать
+                    фрилансов и примеров работ немного
+                </div>
+                <div className="text">Создаю лендинги, прочие одностраничные и многостраничные сайты, порталы, блоги,
+                    магазины, интересные сервисы и так далее
+                </div>
+            </div>
+            <StackFront/>
+            <StackBack/>
         </div>
     }
 }
