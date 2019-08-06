0e4ed5cf54961861f35818ed25480c34579b0062
ivansey <van.secret2015@gmail.com>
2019-08-06 20:19:47 +0300

    Add menu

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 class Header extends React.Component {
     render() {
         return <div className="header">
             <h1 className="title">IvanSey Landing Page</h1>
             <h3 className="desc">Это мой небольшой лендинг. Тут я опишу, чем я занимаюсь, с чем работаю.</h3>
-            <a href="http://ivansey.ru">Открыть мой блог</a>
+            <div className="menu">
+                <Link to="/">Обо мне</Link>
+                <Link to="/example">Примеры работ</Link>
+                <Link to="/reviews">Отзывы</Link>
+            </div>
         </div>
     }
 }
