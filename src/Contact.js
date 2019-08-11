a2025fecf0dd8c72d4c281ba7ed972d9ffc70115
ivansey <van.secret2015@gmail.com>
2019-08-11 21:02:34 +0300

    Change links on icons in Contacts

--- src/Contact.js
+++ src/Contact.js
@@ -5,10 +5,10 @@ class Contact extends React.Component {
         return <div className="page">
             <h2 className="title">Связь со мной</h2>
             <div className="list">
-                <a href="https://vk.com/ivansey2">ВКонтакте</a>
-                <a href="mailto://van.secret2015@gmail.com">E-Mail</a>
-                <a href="https://teleg.run/ivansey">Telegram</a>
-                <a href="https://github.com/ivansey">GitHub</a>
+                <a href="https://vk.com/ivansey2"><span className="mdi mdi-vk"/></a>
+                <a href="mailto://van.secret2015@gmail.com"><span className="mdi mdi-at"/></a>
+                <a href="https://teleg.run/ivansey"><span className="mdi mdi-telegram"/></a>
+                <a href="https://github.com/ivansey"><span className="mdi mdi-github-circle"/></a>
             </div>
         </div>
     }
