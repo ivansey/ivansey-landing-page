24beb3b8433a0734f6916bc7ee0a88020c284989
ivansey <van.secret2015@gmail.com>
2019-12-24 22:45:20 +0300

    Add type in technology model

--- backend/models/technology.js
+++ backend/models/technology.js
@@ -4,6 +4,7 @@ let technologySchema = new mongoose.Schema({
     name: String,
     description: String,
     icon: String,
+    type: String,
 });
 
 let technologyModel = mongoose.model("technology", technologySchema);
